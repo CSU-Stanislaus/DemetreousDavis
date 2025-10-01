14a6f4c2886c5a66470caf548f065cdd3a31ebe9
unknown <ddavis18@csustan.edu>
2025-10-01 10:18:50 -0700

    lab corrected

--- src/B_IntroToJava/StringManipulationChallenge.java
+++ src/B_IntroToJava/StringManipulationChallenge.java
@@ -85,7 +85,7 @@ public class StringManipulationChallenge {
      */
     public static String stringToUpper(String myString) { // the method itself has 'parameters'
         // TODO: Implement this method
-        return stringToUpper(myString);
+        return myString.toUpperCase();
     }
 
     /**
@@ -97,7 +97,7 @@ public class StringManipulationChallenge {
      */
     public static String stringToLower(String usersString) {
         // TODO: Implement this method
-        return stringToLower(usersString);
+        return usersString.toLowerCase();
     }
 
     /**
