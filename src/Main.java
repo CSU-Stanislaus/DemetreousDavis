0c6a73f949b0c670e05ebf5a6dda3294363ec212
unknown <ddavis18@csustan.edu>
2025-11-07 10:56:09 -0800

    Lab 4 completed

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    private static final Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!%n");
+
+
+    }
+}
+
